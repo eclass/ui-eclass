75f1b454e277bf48bce8c7e74b185b24034005d3
Josefin Ferrada <jferrada@eclass.cl>
2019-10-15 17:05:56 -0300

    despliegue

--- src/components/TooltipTrigger/TooltipTrigger.stories.js
+++ src/components/TooltipTrigger/TooltipTrigger.stories.js
@@ -4,20 +4,16 @@ import  TooltipTrigger  from './TooltipTrigger';
 import ProgressIcon from '../ProgressIcon/ProgressIcon';
 import variables from '../variables'
 
-
-
 storiesOf('TooltipTrigger', module)
     .addDecorator(story => <div style={{ display: 'flex' }}>{story()}</div>)
     .add('Top', () => <>
         <ProgressIcon color="white" bgcolor={variables.setColors.deepSkyBlue} icon={"in-progress"} className={`Timeline__item__icon `} id={`Tooltip-in-progress`}/>
         <TooltipTrigger trigger="click" target={"in-progress"}>
-        Para continuar debes completar la unidad anterior </TooltipTrigger>
+        Para continuar debes completar la unidad anterio </TooltipTrigger>
         <ProgressIcon color="white" bgcolor={variables.setColors.deepSkyBlue} icon={"opened"} id={`Tooltip-opened`} />
         <TooltipTrigger placement={"right"}trigger="hover" target={"opened"}>
         Unidad Liberada</TooltipTrigger>
     </>)
-
-    
 
 
 
