d71cdc6dfc5550aaa9d9ba4e288588a107c26dc3
Ender Bonnet <enbonnet@gmail.com>
2019-10-16 14:24:28 -0300

    fix styles and remove serviceworker

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import Button from './Button';
-import { withA11y } from '@storybook/addon-a11y';//accesibilidad addon
-
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import Button from './Button'
+import { withA11y } from '@storybook/addon-a11y' // accesibilidad addon
 
 storiesOf('Button', module)
   .addDecorator(withA11y)
   .addDecorator(story => <div style={{ display: 'flex' }}>{story()}</div>)
-  .add('All', () => <>
-  <Button state="primary" text={"Button Primary"} onClick={action('click')} />
-  <Button  text={"Button Ghost"} ghost={true}  onClick={action('click')}/>
-  <Button state="" text={"Disabled"} disabled={true} onClick={action('click')}/>
-</>)
-  .add('Primary', () => <Button state="primary" text={"Button Primary"} onClick={action('click')}/>)
-  .add('Ghost', () => <Button  text={"Button Ghost"} ghost={true}  onClick={action('click')}/>)
-  .add('Disabled', () => <Button state="" text={"Disabled"} disabled={true} onClick={action('click')}/>)
-  
-
+  .add('All', () => (
+    <>
+      <Button
+        state="primary"
+        text={'Button Primary'}
+        onClick={action('click')}
+      />
+      <Button text={'Button Ghost'} ghost={true} onClick={action('click')} />
+      <Button
+        state=""
+        text={'Disabled'}
+        disabled={true}
+        onClick={action('click')}
+      />
+    </>
+  ))
+  .add('Primary', () => (
+    <Button state="primary" text={'Button Primary'} onClick={action('click')} />
+  ))
+  .add('Ghost', () => (
+    <Button text={'Button Ghost'} ghost={true} onClick={action('click')} />
+  ))
+  .add('Disabled', () => (
+    <Button
+      state=""
+      text={'Disabled'}
+      disabled={true}
+      onClick={action('click')}
+    />
+  ))
--- src/components/ProgressIcon/ProgressIcon.stories.js
+++ src/components/ProgressIcon/ProgressIcon.stories.js
@@ -1,27 +1,92 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import ProgressIcon from './ProgressIcon';
-import { withA11y } from '@storybook/addon-a11y';//accesibilidad addon
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import ProgressIcon from './ProgressIcon'
+import { withA11y } from '@storybook/addon-a11y' // accesibilidad addon
 import variables from '../variables'
 
-
 storiesOf('ProgressIcon', module)
-    .addDecorator(withA11y)
-    .addDecorator(story => <div style={{ display: 'flex', padding: '2em', justifyContent: "space-around" }}>{story()}</div>)
-    .add('All', () => <>
-    <ProgressIcon color="white" bgcolor={variables.setColors.deepSkyBlue} icon={"in-progress"} />
-    <ProgressIcon color="white" bgcolor={variables.setColors.deepSkyBlue} icon={"in-progress"} />
-    <ProgressIcon color="white" bgcolor={variables.setColors.deepSkyBlue} icon={"opened"} />
-    <ProgressIcon color="white" bgcolor={"orange"} icon={"blocked"}/>
-    <ProgressIcon color="white" bgcolor={"#5E7A8C"} icon={"done"}/>
-    <ProgressIcon color="white" bgcolor={variables.setColors.veryLightPink} icon={"blocked"}/>
-    </>)
-    .add('In-progress', () => <ProgressIcon size={"40px"} color="white" bgcolor={variables.setColors.deepSkyBlue} icon={"in-progress"}  />)
-    .add('Current', () => <ProgressIcon size={"40px"} color="white" bgcolor={variables.setColors.deepSkyBlue} icon={"in-progress"} />)
-    .add('Opened', () => <ProgressIcon size={"40px"} color="white" bgcolor={variables.setColors.deepSkyBlue} icon={"opened"} />)
-    .add('Blocked', () => <ProgressIcon size={"40px"} color="white" bgcolor={"orange"} icon={"blocked"}/>)
-    .add('Done', () => <ProgressIcon size={"40px"} color="white" bgcolor={"#5E7A8C"} icon={"done"}/>)
-    .add('Disable', () => <ProgressIcon size={"40px"} color="white" bgcolor={variables.setColors.veryLightPink} icon={"blocked"}/>)
-
-
-    
+  .addDecorator(withA11y)
+  .addDecorator(story => (
+    <div
+      style={{
+        display: 'flex',
+        padding: '2em',
+        justifyContent: 'space-around'
+      }}>
+      {story()}
+    </div>
+  ))
+  .add('All', () => (
+    <>
+      <ProgressIcon
+        color="white"
+        bgcolor={variables.setColors.deepSkyBlue}
+        icon={'in-progress'}
+      />
+      <ProgressIcon
+        color="white"
+        bgcolor={variables.setColors.deepSkyBlue}
+        icon={'in-progress'}
+      />
+      <ProgressIcon
+        color="white"
+        bgcolor={variables.setColors.deepSkyBlue}
+        icon={'opened'}
+      />
+      <ProgressIcon color="white" bgcolor={'orange'} icon={'blocked'} />
+      <ProgressIcon color="white" bgcolor={'#5E7A8C'} icon={'done'} />
+      <ProgressIcon
+        color="white"
+        bgcolor={variables.setColors.veryLightPink}
+        icon={'blocked'}
+      />
+    </>
+  ))
+  .add('In-progress', () => (
+    <ProgressIcon
+      size={'40px'}
+      color="white"
+      bgcolor={variables.setColors.deepSkyBlue}
+      icon={'in-progress'}
+    />
+  ))
+  .add('Current', () => (
+    <ProgressIcon
+      size={'40px'}
+      color="white"
+      bgcolor={variables.setColors.deepSkyBlue}
+      icon={'in-progress'}
+    />
+  ))
+  .add('Opened', () => (
+    <ProgressIcon
+      size={'40px'}
+      color="white"
+      bgcolor={variables.setColors.deepSkyBlue}
+      icon={'opened'}
+    />
+  ))
+  .add('Blocked', () => (
+    <ProgressIcon
+      size={'40px'}
+      color="white"
+      bgcolor={'orange'}
+      icon={'blocked'}
+    />
+  ))
+  .add('Done', () => (
+    <ProgressIcon
+      size={'40px'}
+      color="white"
+      bgcolor={'#5E7A8C'}
+      icon={'done'}
+    />
+  ))
+  .add('Disable', () => (
+    <ProgressIcon
+      size={'40px'}
+      color="white"
+      bgcolor={variables.setColors.veryLightPink}
+      icon={'blocked'}
+    />
+  ))
